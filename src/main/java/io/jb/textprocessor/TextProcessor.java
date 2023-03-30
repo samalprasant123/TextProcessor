e42b0557ea249dd307fca96b5c1578926c3225e0
rightbil <rightbil@gmail.com>
2023-03-30 07:29:04 -0400

    Menu and sort added

--- src/main/java/io/jb/textprocessor/TextProcessor.java
+++ src/main/java/io/jb/textprocessor/TextProcessor.java
@@ -1,9 +1,55 @@
 package io.jb.textprocessor;
-
 import io.jb.textprocessor.util.TextProcessorUtil;
 
+import java.util.Scanner;
 public class TextProcessor {
     public static void main(String[] args) {
-        TextProcessorUtil.printMenu();
+        Scanner scanner = new Scanner(System.in);
+        char wantToContinue;
+        do {
+            TextProcessorUtil.printMenu();
+            System.out.println("\t Enter your choice >>>");
+            String userInput = scanner.nextLine();
+            char choose = userInput.toLowerCase().charAt(0);
+            switch (choose) {
+                case 's':
+                case '1':
+
+                    System.out.println("filePathname?");
+                    System.out.println("enter your order (asc/des)");
+                    String fileName="";
+                    String order="";
+                    TextProcessorUtil.sort(fileName, order);
+                    System.out.println("sort");
+                    break;
+                case 'r':
+                case '2':
+                    System.out.println("remove");
+                    break;
+                case 'f':
+                case '3':
+                    System.out.println("find");
+                    break;
+                case 'p':
+                case '4':
+                    System.out.println("replace");
+                    break;
+                case 'm':
+                case '5':
+                    System.out.println("merge");
+                    break;
+                case 'e':
+                case '6':
+                    System.out.println("Bye, you are exiting");
+                default:
+                    System.out.println("unknown choice");
+            }
+            System.out.println("press (6 or e) to exit or (c) to continue the program");
+            wantToContinue = scanner.nextLine().charAt(0);
+
+        } while (wantToContinue != 'e' || wantToContinue != '6');
+        System.out.println("Bye! Exiting text processing application");
+
     }
+
 }
--- src/main/java/io/jb/textprocessor/util/TextProcessorUtil.java
+++ src/main/java/io/jb/textprocessor/util/TextProcessorUtil.java
@@ -1,12 +1,24 @@
 package io.jb.textprocessor.util;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class TextProcessorUtil {
-
     public static void printMenu() {
         System.out.println();
-        System.out.println("        +----------+");
-        System.out.println("        |   MENU   |");
-        System.out.println("        +----------+");
-        // Menu options goes here
+        System.out.println("\t+----------------------------------------------------------+");
+        System.out.println("\t|            MENU-Text Processor                           |");
+        System.out.println("\t+----------------------------------------------------------+");
+        System.out.println("\t|press |1 or s: sort alphabetically (Ascending/Descending       |");
+        System.out.println("\t|      |2 or r: remove duplicate                               |");
+        System.out.println("\t|      |3 or f: finding text and indicate line number in a file |");
+        System.out.println("\t|      |4 or p: replace a specific text with new text           |");
+        System.out.println("\t|      |5 or m: merge files into a single file                  |");
+        System.out.println("\t|------|6 or e: exit the program--------------------------------|");
     }
+
+    public static void sort(String filePath, String order ){
+        //TODO Bili
+
+    }
+
+
 }
